7d8a7b6b81db29885cbb34c689a5ddd1904182a3
Akhil Vinayak P <46107438+AkhilVinayakp@users.noreply.github.com>
2019-05-05 12:07:31 +0530

    creating  user module

--- app/user.js
+++ app/user.js
@@ -1,1 +1,7 @@
-
+var user = angular.module("user",['ngRoute']);
+user.config(function ($routeProvider) {
+   $routeProvider.when("/",{
+       templateUrl:"partials/usr_log.html",
+       controller:"logCtrl"
+   })
+});
