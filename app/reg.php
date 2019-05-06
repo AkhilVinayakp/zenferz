e23181cafeecfe38ec6d7ef9442bba4d2027ead5
Akhil Vinayak P <46107438+AkhilVinayakp@users.noreply.github.com>
2019-05-06 18:10:23 +0530

    initial

---
+++ app/reg.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User:
+ * Date: 2/8/2019
+ * Time: 1:48 AM
+ */
+require "database.php";
+$database=new database();
+$data=array();
+$status=0;
+/* connection req*/
+
+?>
