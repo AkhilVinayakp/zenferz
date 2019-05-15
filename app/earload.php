03f5b0f047b5f8ae00505a2e67ec8ce777679be2
Akhil Vinayak P <46107438+AkhilVinayakp@users.noreply.github.com>
2019-05-15 08:24:48 +0530

    normal function call response

--- app/earload.php
+++ app/earload.php
@@ -11,3 +11,35 @@ $data=array();
 $like=array();
 $liked=null;
 $database=new database();
+if($postdata->fn=="normal") {
+
+
+    $database->query('select * from event where status=0 order by EARId desc ');
+    $result = $database->resultset();
+    foreach ($result as $row) {
+        $data[] = $row;
+    }
+//finding the liked ear id
+    $database->query('select liked from user where uid=:uid');
+    $database->bind(':uid', $postdata->uid);
+    $liked = $database->resultset();
+    if ($liked) {
+        $uid = $liked[0]->liked;
+        $n = strlen($uid);
+        $i = 0;
+        while ($i < $n) {
+            $like[] = (int)substr($uid, $i, 3);
+            $i = $i + 4;
+        }
+    } else
+        $like = 0;
+}
+
+
+$output=array(
+    'output' => $data,
+    'liked'  =>$like
+);
+
+ echo json_encode($output);
+
